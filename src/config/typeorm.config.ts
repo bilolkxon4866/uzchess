c40bd6b73502b4291c3d450ddb3812b5d3c98c05
Bilolxon Xolmamatov <xolmamatovbilolxon2@gmail.com>
2026-04-03 17:56:30 +0500

    first commit

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,9 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  url: 'postgresql://postgres:123@localhost:5432/uzchess',
+  synchronize: true,
+  dropSchema: true,
+  entities: ['dist/**/*.entity.js'],
+}
